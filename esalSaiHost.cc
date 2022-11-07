58c2dbd002734ec259de2b4184a25bec44e80655
Simmons, Steven <simmonss@localhost>
2022-11-07 10:44:05 -0600

    BLADESW-424 add BPDU Rx

--- esalSaiHost.cc
+++ esalSaiHost.cc
@@ -152,7 +152,7 @@ static void convertMacStringToAddr(std::string &macString,
 
 static bool searchFilterTable(
     uint32_t lPort, int &idx, const void *buffer, sai_size_t bufferSz) {
-    const char *bufPtr = (const char*) buffer;
+    const unsigned char *bufPtr = (const unsigned char*) buffer;
     unsigned char macAddr[MAC_SIZE];
  
     // Check to see that the buffer is bigger than minimum size
@@ -160,16 +160,25 @@ static bool searchFilterTable(
         return false;
     }
 
+    // Buffer is packet with the following format:
+    //    
+    //    DST MAC: 6 bytes
+    //    SRC MAC: 6 bytes
+    //    VLAN Type: 2 bytes ... must be 0x8100
+    //    VLAN ID: 2 bytes
+    //    Ether Type for packet : 2 bytes
+
     // Assuming packet starts at DST MAC.
     //     DSTMAC[6] SRCMAC[6] ETYPE[2] VLANID[2]
+   
     memcpy(macAddr, buffer, MAC_SIZE);
  
     // Get VLAN. 
     uint16_t vlan = 0;
-    char *vlanPtr = (char*) &vlan; 
+    unsigned char *vlanPtr = (unsigned char*) &vlan; 
     if ((bufPtr[12] == 0x81) && (bufPtr[13] == 0x00)) {
-        vlanPtr[0] = bufPtr[12];
-        vlanPtr[1] = bufPtr[13];
+        vlanPtr[0] = bufPtr[14];
+        vlanPtr[1] = bufPtr[15];
         vlan = ntohs(vlan);
         vlan &= 0xfff; 
     }
@@ -185,7 +194,7 @@ static bool searchFilterTable(
         // Check to see if logical port matches.
         auto vpsize = fltr.vendorport_size(); 
         if (vpsize) {
-            bool matching = false;
+            matching = false;
             for(int vpidx = 0; vpidx < vpsize; vpidx++) {
                 if (fltr.vendorport(vpidx) == lPort) {
                     matching = true;
@@ -228,15 +237,15 @@ static bool searchFilterTable(
         // Check Raw Data
         if (fltr.rawdata_size() && matching) {
             for (auto i = 0; i < fltr.rawdata_size(); i++){
-                const char *bufPtr = (const char*) buffer;
+                unsigned const char *bufPtr = (unsigned const char*) buffer;
                 auto offset = fltr.rawdata(i).offset(); 
-                if ((offset+sizeof(long))  > bufferSz) {
+                if ((offset*sizeof(long))  > bufferSz) {
                     matching = false; 
                     break; 
                 }
                 auto data = fltr.rawdata(i).data(); 
                 auto mask = fltr.rawdata(i).mask(); 
-                const char *pktPtr = bufPtr+offset;
+                unsigned const char *pktPtr = bufPtr+offset;
 
                 // Coerce to longword alignment.
                 // Assuming mask handles bufsize that
@@ -245,6 +254,10 @@ static bool searchFilterTable(
                     (pktPtr[offset] << 24) | (pktPtr[offset+1] << 16) | 
                     (pktPtr[offset+2] << 8) | (pktPtr[offset+3]);
 
+                pkt = ntohl(pkt);
+                
+                // Compare the packet contents masked with the raw data masked. 
+                //  
                 if ((pkt & mask) != (data & mask)) {
                     matching = false;
                     break;
@@ -269,7 +282,7 @@ bool esalHandleSaiHostRxPacket(const void *buffer,
     // Check to see if callback is registered yet. 
     if (!rcvrCb) {
         return false;
-    }
+    } 
 
     // Find the incoming port.
     sai_object_id_t portSai = SAI_NULL_OBJECT_ID;
@@ -410,6 +423,7 @@ int VendorDeletePacketFilter(const char *filterName) {
 }
 
 int VendorSendPacket(uint16_t lPort, uint16_t length, const void *buf) {
+
 #ifndef UTS
     sai_status_t retcode = SAI_STATUS_SUCCESS;
     sai_hostif_api_t *sai_hostif_api;
